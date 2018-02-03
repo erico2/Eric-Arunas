4b0080e26647485b06cf4413d5a07279047792f9
Hawkubite <36006075+Hawkubite@users.noreply.github.com>
2018-02-03 15:41:33 +0200

    Create location.java

---
+++ City_of_Aaron_model_layer/src/byui/cit260/cityOfAaron/model/location.java
@@ -0,0 +1,38 @@
+	package byu.cit260.cityOfAaron.model;
+	
+
+	/**
+	 *
+	 * @author Arunas Rancevas
+	 */
+	 //
+	
+
+	 import java.io.Serializable;
+	
+
+	 public class Location implements Serializable{
+	   //attribute
+	   private String description;
+	   private String symbol;
+	   //constructor
+	   public Location(){}
+	   //getter and setter
+	   public String getDescription(){
+	     return description;
+	   }
+	   public void setDescription(String description){
+	     this.description = description;
+	   }
+	   public String getSymbol(){
+	     return symbol;
+	   }
+	   public void setSymbol(String symbol){
+	       this.symbol = symbol;
+	   }
+	   @Override
+	   public String toString() {
+	       return "You are in " + description;
+	   }
+	 }
+
