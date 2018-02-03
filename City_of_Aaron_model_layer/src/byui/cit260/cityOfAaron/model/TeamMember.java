9885a25f7150619c9a1861bdcab38008a1c459de
erico2 <okoro_eric@ymail.com>
2018-02-03 07:56:20 +0000

    Game teammember class name

---
+++ City_of_Aaron_model_layer/src/byui/cit260/cityOfAaron/model/TeamMember.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.cityOfAaron.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author LocalClerk
+ */
+public class TeamMember implements Serializable{
+    // class instance variables
+    private String name;
+    private String title;
+
+    public TeamMember() {
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.title);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "TeamMember{" + "name=" + name + ", title=" + title + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TeamMember other = (TeamMember) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
